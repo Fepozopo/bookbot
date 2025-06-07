951eb60b30c6361883f06b6721db7802c211e2a1
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-06-06 18:55:25 -0700

    Add stats module with word and character counting functions

---
+++ stats.py
@@ -0,0 +1,22 @@
+# Count words of text
+def count_words(text):
+    w = text.split()
+    words = len(w)
+
+    return words
+
+
+# Count characters of text
+def count_characters(text):
+    lower_text = text.lower()
+    characters = {}
+    for i in lower_text:
+        if i in characters and i.isalpha():
+            characters[i] += 1
+        elif i.isalpha():
+            characters[i] = 1
+
+    sorted_characters = sorted(characters.items(), key=lambda x: x[1], reverse=True)
+
+    for k in sorted_characters:
+        print(f"The '{k[0]}' character was found {k[1]} times.")
